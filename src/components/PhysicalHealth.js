ff371dfd52bcfae7a2d9f6664e0e07dac1cc0350
MatthewGuyRobin <matthewguyrobinson@gmail.com>
2020-04-08 12:10:52 +0100

    physicalhealth

---
+++ src/components/PhysicalHealth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PhysicalHealthCard from './PhysicalHealthCard'
+
+export default function PhysicalHealth() {
+
+  return (
+    <div className="header-container">
+      <h1>Humour Health Service</h1>
+      <h2 className="headerquote">A positive view on trying times</h2>
+      <PhysicalHealthCard/>
+    </div>
+  )
+}
