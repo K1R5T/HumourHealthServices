7c77b07a34eefe140df74dae27ce6f3ec1e22666
MatthewGuyRobin <matthewguyrobinson@gmail.com>
2020-04-08 16:14:18 +0100

    physicaltipsadded

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 
-export default function Navbar(props) {
+export default function Navbar() {
     return (
         <div className="navbarContainer">
             <nav className="navbar">
